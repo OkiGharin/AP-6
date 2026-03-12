454987c52d9090e4cd9076ea45a57b5a21c9026a
OkiGharin <gharinshauqi9@gmail.com>
2026-03-12 14:49:52 +0700

    Add files via upload

---
+++ Pertemuan 4/tugasNestedFor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    system ("cls");
+
+    for(int i = 1; i <= 7; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
